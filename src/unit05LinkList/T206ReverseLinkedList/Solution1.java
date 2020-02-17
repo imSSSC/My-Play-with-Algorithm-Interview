0329eac94392f53bf5c6390ac54e195cc56dd5aa
snake <824475386@qq.com>
2020-02-17 18:38:27 +0800

    链表翻转  递归,迭代两种方式完成

---
+++ src/unit05LinkList/T206ReverseLinkedList/Solution1.java
@@ -0,0 +1,43 @@
+package unit05LinkList.T206ReverseLinkedList;
+
+/**
+ * @auther: snake
+ * @date: 2020/2/17 17:33
+ */
+
+// 206. 反转链表
+// https://leetcode-cn.com/problems/reverse-linked-list/
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+public class Solution1 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            // 将下个节点指针,移动一位(获取下个节点指针)
+            ListNode next = cur.next;
+
+            // 指针方向改变
+            cur.next = pre;
+
+            // 将前节点指针,指向当前节点 移动一位
+            pre = cur;
+            // 将当前节点指针,指向下个节点 移动一位
+            cur = next;
+        }
+
+        return pre;
+    }
+
+
+}
